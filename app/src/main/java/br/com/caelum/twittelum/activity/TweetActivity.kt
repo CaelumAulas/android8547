44344b9747652f69e7873cfad818fabe668d3460
Ana Losnak <ana.losnak@caelum.com.br>
2019-11-16 13:50:19 -0300

    Mostra foto na tela de criação

--- app/src/main/java/br/com/caelum/twittelum/activity/TweetActivity.kt
+++ app/src/main/java/br/com/caelum/twittelum/activity/TweetActivity.kt
@@ -1,12 +1,20 @@
 package br.com.caelum.twittelum.activity
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProviders
 import br.com.caelum.twittelum.R
 import br.com.caelum.twittelum.bancodedados.TweetDao
@@ -15,16 +23,34 @@ import br.com.caelum.twittelum.modelo.Tweet
 import br.com.caelum.twittelum.repository.TweetRepository
 import br.com.caelum.twittelum.viewmodel.TweetViewModel
 import br.com.caelum.twittelum.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_tweet.*
+import java.io.File
 
 class TweetActivity : AppCompatActivity() {
     private val tweetViewModel: TweetViewModel by lazy { ViewModelProviders.of(this, ViewModelFactory)
                                 .get(TweetViewModel::class.java) }
+    var caminhoDaFoto: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweet)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (caminhoDaFoto != null) {
+            carregaImagem()
+        }
+    }
+
+    private fun carregaImagem() {
+        val bitmap = BitmapFactory.decodeFile(caminhoDaFoto)
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, true)
+        tweet_foto.setImageBitmap(scaledBitmap)
+        tweet_foto.scaleType = ImageView.ScaleType.FIT_XY
     }
 
     private fun publicaTweet() {
@@ -50,6 +76,17 @@ class TweetActivity : AppCompatActivity() {
             R.id.salva -> {
                 publicaTweet()
                 finish()
+            }
+            R.id.camera -> {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                caminhoDaFoto = "${getExternalFilesDir(Environment.DIRECTORY_PICTURES)}/${System.currentTimeMillis()}.jpg"
+                Log.i("caminho", caminhoDaFoto)
+                val arquivo = File(caminhoDaFoto)
+                val uri =
+                    FileProvider.getUriForFile(this,"br.com.caelum.twittelum.fileprovider", arquivo)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+                startActivity(intent)
             }
             android.R.id.home -> finish()
         }
